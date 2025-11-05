4a2ba2e45e773fd90e007d194667da25fe5c8f5e
ANKU890 <84028454+ANKU890@users.noreply.github.com>
2025-11-05 13:07:01 +0530

    Add files via upload

---
+++ components/icons/BackspaceIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BackspaceIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zM18 9l-6 6M12 9l6 6" />
+    </svg>
+);
+
+export default BackspaceIcon;
---
+++ components/icons/GasPumpIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GasPumpIcon: React.FC = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m-7 13V13m0 0l-3 3m3-3l3 3m5-11V4a1 1 0 00-1-1h-2a1 1 0 00-1 1v3" />
+    </svg>
+);
+
+export default GasPumpIcon;
